1c7b386cef783927b2ea992d22d32444c35f8d3b
bfresnel <benoit.fresnel.1@gmail.com>
2020-05-11 15:42:54 +0200

    Add PropertiesName to Json file

--- JdkSwitcher/Model/PathConfiguration.cs
+++ JdkSwitcher/Model/PathConfiguration.cs
@@ -1,8 +1,12 @@
-﻿namespace JdkSwitcher
+﻿using System.Text.Json.Serialization;
+
+namespace JdkSwitcher
 {
     class PathConfiguration
     {
+        [JsonPropertyName("Name")]
         public string Name { get; set; }
+        [JsonPropertyName("PathValue")]
         public string PathValue { get; set; }
 
         public PathConfiguration()
